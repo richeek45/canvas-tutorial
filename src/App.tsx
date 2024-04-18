cd10aed3fcf2f7f0b855cd6227a0ae38c0ead908
richeek45 <richeek45@gmail.com>
2024-04-18 18:46:11 +0530

    made canvas full screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,32 @@
 
 import { useEffect } from 'react';
 import './App.css'
+import { draw } from './utils/draw';
+import { init } from './utils/animations';
+
+const createText = (ctx: CanvasRenderingContext2D) => {
+  ctx.font = "60px serif";
+  // ctx.fillText("Hello world", 20, 120, 200);
+  ctx.strokeText("Hello world", 20, 120, 200);
+}
 
 function App() {
 
-
-  function draw() {
-    const canvas = document.getElementById("tutorial") as HTMLCanvasElement;
-    if (canvas.getContext) {
-      const ctx = canvas.getContext("2d")
-      if (ctx) {
-        // ctx.fillStyle = "rgb(200, 0, 0)";
-        // ctx.fillRect(10, 10, 50, 50);
-
-        // ctx.fillStyle = "rgb(0 0 200 / 50%)";
-        // ctx.fillRect(30, 30, 50, 50)
-
-        // Drawing three rectangle
-        // ctx.fillRect(25, 25, 100, 100);
-        // ctx.clearRect(45, 45, 60, 60);
-        // ctx.strokeRect(50, 50, 50, 50);
-
-        // Drawing a triangle 
-        // ctx.beginPath();
-        // ctx.moveTo(75, 50);
-        // ctx.lineTo(100, 75);
-        // ctx.lineTo(100, 25);
-        // ctx.fill();
-        
-        // Drawing a smiley face
-        // ctx.beginPath();
-        // ctx.arc(100, 75, 50, 0, 2 * Math.PI, true);
-        // ctx.moveTo(90, 60);
-        // ctx.arc(80, 60, 10, 0, 2 * Math.PI, true);
-        // ctx.moveTo(130, 60);
-        // ctx.arc(120, 60, 10, 0 , 2 * Math.PI, true);
-        // ctx.moveTo(135, 75);
-        // ctx.arc(100, 75, 35, 0, 1 * Math.PI, false); // mouth
-        // ctx.stroke();
-
-        // // Drawing two adjacent triangles
-        // // 1. Filled triangle
-        // ctx.beginPath();
-        // ctx.moveTo(20, 20);
-        // ctx.lineTo(100, 20);
-        // ctx.lineTo(20, 100);
-        // ctx.fill();
-
-        // // 2. Stroked triangle
-        // ctx.beginPath();
-        // ctx.moveTo(120, 120);
-        // ctx.lineTo(120, 40);
-        // ctx.lineTo(40, 120);
-        // ctx.closePath();
-        // ctx.stroke();
-
-        
-
-      }
-    }
-  }
   
   useEffect(() => {
-    window.addEventListener("load", draw);
+    // window.addEventListener("load", draw);
     
-    return () => window.removeEventListener("load", draw)
+    // return () => window.removeEventListener("load", draw)
+    window.addEventListener("load", init);
+    
+    return () => window.removeEventListener("load", init)
   }, [])
 
   return (
     <>
-      <div>
-      <canvas width="500" height="500" style={{ background: "lightgreen"}} id="tutorial">
+      <canvas style={{ background: "black"}} id="tutorial">
       Testing Canvas Drawing
      </canvas>
-      </div>
     </>
   )
 }
