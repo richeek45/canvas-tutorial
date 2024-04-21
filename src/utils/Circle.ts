b6d9c89497702296017c6ea2c18b3a4d328e524b
richeek45 <richeek45@gmail.com>
2024-04-21 19:48:57 +0530

    added collision resolution and mouse effect change in opacity

--- src/utils/Circle.ts
+++ src/utils/Circle.ts
@@ -1,4 +1,5 @@
 import { mouse } from "./animations";
+import { getDistance, particleCollision } from "./helper";
 
 const maxRadius = 40;
 const speed = 2;
@@ -10,8 +11,10 @@ export class Circle {
   dx: number;
   dy: number;
   r: number;
+  mass: number;
   minR: number;
   color: string;
+  opacity: number;
   ctx: CanvasRenderingContext2D;
   canvas: HTMLCanvasElement
 
@@ -30,6 +33,8 @@ export class Circle {
     this.r = r;
     this.dx = dx;
     this.dy = dy;
+    this.mass = 1;
+    this.opacity = 0;
 
     this.color = color;
     this.ctx = ctx;
@@ -40,8 +45,15 @@ export class Circle {
   draw() {
     this.ctx.beginPath();
     this.ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+
+    this.ctx.save();
+    this.ctx.globalAlpha = this.opacity;
     this.ctx.fillStyle = this.color;
     this.ctx.fill();
+    this.ctx.restore();
+
+    this.ctx.strokeStyle = this.color;
+    this.ctx.stroke();
   }
 
   gravity() {
@@ -64,10 +76,43 @@ export class Circle {
 
   }
 
-  update() {
+  hasCollidedMouseEvent() {
+    const xProximity = mouse.x && ((this.x - mouse.x) < 50) && ((this.x - mouse.x) > -50);
+    const yProximity = mouse.y && ((this.y - mouse.y) < 50) && ((this.y - mouse.y) > -50)
+    if (xProximity && yProximity && this.r < maxRadius) {
+      this.ctx.fillStyle = "red";
+      this.r += 1;
+    } else if (this.r > this.minR) {
+      this.r -= 2;
+    }
+  }
+
+
+  updateCollision(circles: Circle[]) {
+    this.draw();
+
+    for(let i = 0; i < circles.length; i++) {
+      if (this === circles[i]) continue; // skip the same circle comparison 
+
+      if ((getDistance(this.x, this.y, circles[i].x, circles[i].y) - (2 * this.r)) < 0) {
+        particleCollision(this, circles[i]);
+      }
+    }
+
+    this.bounceWalls();
+  }
+
+  mouseCollisionDetection() {
+    if ((getDistance(mouse.x, mouse.y, this.x, this.y) - (2 * this.r)) < 150 && this.opacity < 0.5) {
+      this.opacity += 0.02;
+    } else if (this.opacity > 0) {
+      this.opacity = Math.max(0, this.opacity - 0.02);
+    }
+  }
+
+  bounceWalls() {
     const canvasWidth = this.canvas.width;
     const canvasHeight = this.canvas.height;
-    this.draw();
 
     if (this.x + this.r > canvasWidth || this.x - this.r < 0) {
       this.dx = - this.dx;
@@ -80,25 +125,15 @@ export class Circle {
     this.x += this.dx;
     this.y += this.dy;
 
-    const xProximity = mouse.x && ((this.x - mouse.x) < 50) && ((this.x - mouse.x) > -50);
-    const yProximity = mouse.y && ((this.y - mouse.y) < 50) && ((this.y - mouse.y) > -50)
-    if (xProximity && yProximity && this.r < maxRadius) {
-      this.ctx.fillStyle = "red";
-      this.r += 1;
-    } else if (this.r > this.minR) {
-      this.r -= 2;
-    }
+    this.mouseCollisionDetection()
 
+  }
+
+  update() {
+    this.draw();
+    this.bounceWalls();
+    this.hasCollidedMouseEvent();
   }
 }
 
 
-export const playground = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, circleArray: Circle[]) => {
-  ctx.globalCompositeOperation = "destination-over";
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
-  for (let i = 0; i < circleArray.length; i++) {
-    circleArray[i].update();
-  }
-
-}
